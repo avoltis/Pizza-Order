4face71cf6a1b1472737a4b60f154b08b85cfbae
vagolli <v.agolli@live.com>
2018-11-13 14:03:29 +0100

    order pizza button

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -10,47 +10,48 @@ class Home extends StatelessWidget {
             color: Colors.deepOrangeAccent,
             child: Column(
               children: <Widget>[
-                 Row(
-              children: <Widget>[
-                Expanded(
-                    child: Text("Margherita",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            decoration: TextDecoration.none,
-                            fontFamily: "Oxygen",
-                            fontWeight: FontWeight.normal))),
-                Expanded(
-                    child: Text("Tomato, Mozzarella, Basil",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            decoration: TextDecoration.none,
-                            fontFamily: "Oxygen",
-                            fontWeight: FontWeight.normal)))
-              ],
-            ),
-             Row(
-              children: <Widget>[
-                Expanded(
-                    child: Text("Marinara",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                            fontSize: 30.0,
-                            decoration: TextDecoration.none,
-                            fontFamily: "Oxygen",
-                            fontWeight: FontWeight.normal))),
-                Expanded(
-                    child: Text("Tomato, Garlic",
-                        textDirection: TextDirection.ltr,
-                        style: TextStyle(
-                            fontSize: 20.0,
-                            decoration: TextDecoration.none,
-                            fontFamily: "Oxygen",
-                            fontWeight: FontWeight.normal)))
-              ],
-            ),
-            PizzaImageWidget()
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text("Margherita",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: "Oxygen",
+                                fontWeight: FontWeight.normal))),
+                    Expanded(
+                        child: Text("Tomato, Mozzarella, Basil",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: "Oxygen",
+                                fontWeight: FontWeight.normal)))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text("Marinara",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 30.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: "Oxygen",
+                                fontWeight: FontWeight.normal))),
+                    Expanded(
+                        child: Text("Tomato, Garlic",
+                            textDirection: TextDirection.ltr,
+                            style: TextStyle(
+                                fontSize: 20.0,
+                                decoration: TextDecoration.none,
+                                fontFamily: "Oxygen",
+                                fontWeight: FontWeight.normal)))
+                  ],
+                ),
+                PizzaImageWidget(),
+                OrderButton()
               ],
             )));
   }
@@ -63,5 +64,34 @@ class PizzaImageWidget extends StatelessWidget {
     Image image = Image(image: pizzaAsset, width: 200.0, height: 200.0);
 
     return Container(child: image);
+  }
+}
+
+class OrderButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(top: 50.0),
+        child: RaisedButton(
+          child: Text("Order your Pizza"),
+          color: Colors.lightGreen,
+          elevation: 5.0,
+          onPressed: () {
+          order(context);
+          },
+        ));
+  }
+
+  void order(BuildContext context) {
+    var alert = AlertDialog(
+      title: Text("Order Completed"),
+      content: Text("Thanks for your order"),
+    );
+
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        });
   }
 }
