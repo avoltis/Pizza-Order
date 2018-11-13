cc6096efb9069081afbe1224fdc486d579c6fc94
vagolli <v.agolli@live.com>
2018-11-13 11:00:52 +0100

    adding custom font

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -7,10 +7,13 @@ class Home extends StatelessWidget {
         child: Container(
       alignment: Alignment.center,
       color: Colors.deepOrangeAccent,
-      // width: 192.0,
-      // height: 96.0,
-      // margin: EdgeInsets.only(left: 50),
-      child: Text("Pizza", textDirection: TextDirection.ltr),
+      child: Text("Pizza",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              fontSize: 80.0,
+              decoration: TextDecoration.none,
+              fontFamily: "Oxygen",
+              fontWeight: FontWeight.normal)),
     ));
   }
 }
